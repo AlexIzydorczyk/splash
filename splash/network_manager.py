9c9d5f6c991b2c4cb94bc75b5b5c7e85bb5a9cad
AlexIzydorczyk <aleks.izydorczyk@gmail.com>
2015-07-17 12:26:23 -0400

    Cache

--- splash/network_manager.py
+++ splash/network_manager.py
@@ -257,7 +257,7 @@ class ProxiedQNetworkAccessManager(QNetworkAccessManager):
 
         data = self.cache().data(reply.url())
 
-        self.log("Data exists..?" + str(data.__class__))
+        self.log("Data exists..?" + str(data.data().__class__))
 
 
         har_entry = self._harEntry()
