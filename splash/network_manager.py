d6ef79ce98d560a8868840a1bff3e8117ca52b52
AlexIzydorczyk <aleks.izydorczyk@gmail.com>
2015-07-17 12:04:52 -0400

    Cache

--- splash/network_manager.py
+++ splash/network_manager.py
@@ -254,11 +254,11 @@ class ProxiedQNetworkAccessManager(QNetworkAccessManager):
         self.log("Bytes available: " + str(reply.bytesAvailable()))
         self.log("Reply read: " + str(len(bytes(reply.readAll()))))
         self.log("Reply buffer size: " + str(reply.readBufferSize()))
-        #self.log("Size of cache in memory: " + str(sys.getsizeof(self.cache())))
+        self.log("Size of cache in memory: " + str(self.cache()).__class__)
 
-        data = self.cache().data(reply.url())
+        #data = self.cache().data(reply.url())
 
-        self.log("Data exists..?" + str(sys.getsizeof(data)))
+        #self.log("Data exists..?" + str(sys.getsizeof(data)))
 
 
         har_entry = self._harEntry()
