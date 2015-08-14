a557b08e35d68cb5e3e0127cd959cf815f2c90ac
AlexIzydorczyk <aleks.izydorczyk@gmail.com>
2015-08-14 10:55:54 -0400

    Add back normal cache

--- splash/cache.py
+++ splash/cache.py
@@ -9,34 +9,34 @@ from splash import defaults
 from PyQt5.QtCore import QByteArray
 
 
-class SplashDiskCache(QNetworkDiskCache):
-
-    def prepare(self, metadata):
-        print "-----------"
-        print "THIS SHOULD BLOCK CACHING!"
-        print metadata.url()
-        metadata.setSaveToDisk(True)
-        print metadata.saveToDisk()
-        metadata.setAttributes(QNetworkRequest.CustomVerbAttribute, QByteArray('GET'))
-        metadata.setAttributes(QNetworkRequest.CacheSaveControlAttribute, True)
-        print "-----------"
-        super(SplashDiskCache, self).prepare(metadata)
-
-
-    def updateMetaData(self, metadata):
-        print "THIS SHOULD BLOCK CACHING FIRST!"
-        print "-----------"
-
-    # def insert(self, QIODevice):
-    #     print "THIS SHOULD BLOCK INSERTING!"
-    #
-    #
-
+# class SplashDiskCache(QNetworkDiskCache):
+#
+#     def prepare(self, metadata):
+#         print "-----------"
+#         print "THIS SHOULD BLOCK CACHING!"
+#         print metadata.url()
+#         metadata.setSaveToDisk(True)
+#         print metadata.saveToDisk()
+#         metadata.setAttributes(QNetworkRequest.CustomVerbAttribute, QByteArray('GET'))
+#         metadata.setAttributes(QNetworkRequest.CacheSaveControlAttribute, True)
+#         print "-----------"
+#         super(SplashDiskCache, self).prepare(metadata)
+#
+#
+#     def updateMetaData(self, metadata):
+#         print "THIS SHOULD BLOCK CACHING FIRST!"
+#         print "-----------"
+#
+#     # def insert(self, QIODevice):
+#     #     print "THIS SHOULD BLOCK INSERTING!"
+#     #
+#     #
+#
 
 
 def construct(path=defaults.CACHE_PATH, size=defaults.CACHE_SIZE):
     log.msg("Initializing cache on %s (maxsize: %d Mb)" % (path, size))
-    cache = SplashDiskCache()
+    cache = QNetworkDiskCache()
     cache.setCacheDirectory(path)
     cache.setMaximumCacheSize(size * 1024**2)
     cache.cacheSize()  # forces immediate initialization
